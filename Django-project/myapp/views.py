b36a458eb93eae5e4490b8ac825683b7dbae41f6
unknown <mbrito31@alumnos.uaq.mx>
2024-05-04 12:52:42 -0600

    se agrego main

---
+++ Django-project/myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+def hello (request):
+    return HttpResponse("<h1>Hello World<h1>")
+
+def about (request):
+    return HttpResponse("<h1>About Us<h1>")
+
+def home (request):
+    return render(request,'home.html')
+
+def newDesing (request):
+    return HttpResponse("<h1>New Desing Page<h1>")
+
+def carShop (request):
+    return HttpResponse("<h1>Card Page<h1>")
